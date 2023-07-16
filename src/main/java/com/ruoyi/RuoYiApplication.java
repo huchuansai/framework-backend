a3ae845cc9f22c324f8246bcb91038850d8a0d0d
hucs <huchuansai@foxmail.com>
2023-07-16 22:37:12 +0800

    feat: init commit

---
+++ src/main/java/com/ruoyi/RuoYiApplication.java
@@ -0,0 +1,31 @@
+package com.ruoyi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+
+/**
+ * 启动程序
+ *
+ * @author ruoyi
+ */
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.ruoyi",
+    excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.REGEX, pattern = "com.ruoyi.oss.core.*"),
+        @ComponentScan.Filter(type = FilterType.REGEX, pattern = "com.ruoyi.system.domain.*"),
+    })
+public class RuoYiApplication {
+
+    public static void main(String[] args) {
+        System.setProperty("spring.devtools.restart.enabled", "false");
+        SpringApplication application = new SpringApplication(RuoYiApplication.class);
+        application.setApplicationStartup(new BufferingApplicationStartup(2048));
+        application.run(args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  RuoYi-Vue-Plus启动成功   ლ(´ڡ`ლ)ﾞ");
+    }
+
+}
